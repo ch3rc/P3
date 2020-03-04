52da796ad296c69485e2267a90dfbbc377b671bb
ch3rc <ch3rc@delmar.umsl.edu>
2020-03-04 16:46:22 -0600

    created three files i think i will need

---
+++ main.c
@@ -0,0 +1,18 @@
+//===============================================================
+//Date:		March 4, 2020
+//Author:	Cody Hawkins
+//Class:	CS4280
+//Project:	P1
+//File:		main.c
+//===============================================================
+
+
+int main(int argc, char *argv)
+{
+
+	//pass arguments to filter
+	//filter will call scanner
+	//filter(argc, argv);
+	
+	return 0;
+}	
