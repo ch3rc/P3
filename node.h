c56a126a7e1e9cef3b7e6d0e14c96adef4eaeb79
ch3rc <ch3rc@delmar.umsl.edu>
2020-04-26 17:32:23 -0500

    finished P3

--- node.h
+++ node.h
@@ -14,10 +14,13 @@ struct Node{
 	struct Node* child;
 	char* token;
 	int size;
+	int id;
 };
 
 struct Node *newNode(char*);
 void getToken(struct Node*, struct Node*);
 void preOrder(struct Node*, int);
+void SSTV(struct Node *);
+void staticSemantics(struct Node *);
 
 #endif
